9c6a59459c7b8bbdd62e25705ff1d55112dd498e
Deepak Azad <dazad>
2013-08-02 16:52:17 -0700

    Implement all println methods

--- DebugWithSysout/src/debugwithsysout/DebugStream.java
+++ DebugWithSysout/src/debugwithsysout/DebugStream.java
@@ -33,6 +33,54 @@ public class DebugStream extends PrintStream {
 		super.println(x);
 	}
 
+	@Override
+	public void println() {
+		showLocation();
+		super.println();
+	}
+
+	@Override
+	public void println(boolean x) {
+		showLocation();
+		super.println(x);
+	}
+
+	@Override
+	public void println(char x) {
+		showLocation();
+		super.println(x);
+	}
+
+	@Override
+	public void println(int x) {
+		showLocation();
+		super.println(x);
+	}
+
+	@Override
+	public void println(long x) {
+		showLocation();
+		super.println(x);
+	}
+
+	@Override
+	public void println(float x) {
+		showLocation();
+		super.println(x);
+	}
+
+	@Override
+	public void println(double x) {
+		showLocation();
+		super.println(x);
+	}
+
+	@Override
+	public void println(char[] x) {
+		showLocation();
+		super.println(x);
+	}
+
 	private void showLocation() {
 		StackTraceElement element = Thread.currentThread().getStackTrace()[4];
 //		super.print(MessageFormat.format("({0}:{1, number,#}) : ", element.getFileName(), element.getLineNumber()));
